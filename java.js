9f8630e9002770be97790043278b1aeaa80148df
joshdoyle2013@outlook.com <josh@jdhreports.co.uk>
2024-11-15 13:39:05 +0000

    set up foundations for the page

---
+++ java.js
@@ -0,0 +1,35 @@
+
+
+//add function
+const addition = function (num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+console.log(addition(14,16));
+//subtract function
+const subtraction = function(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+console.log(subtraction(13,7));
+//multiply function
+const multiplication = function(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+console.log(multiplication(13,7));
+//divide function
+const division = function (num1, num2) {
+    let result = num1/num2;
+    return result;
+}
+
+console.log(division(4,2));
+
+//create variables for number, operator and another number
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';

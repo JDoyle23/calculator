f452a766d10c9dd34f185631b7735417386809b2
joshdoyle2013@outlook.com <josh@jdhreports.co.uk>
2024-11-21 13:24:03 +0000

    debug and declutter the js page

--- java.js
+++ java.js
@@ -1,76 +1,77 @@
+//functions to ensure it can do basic operations
 //add function
 const addition = function (num1, num2) {
-    let result = num1 + num2;
-    return result;
+  let result = num1 + num2;
+  return result;
 }
 //subtract function
 const subtraction = function(num1, num2) {
-    let result = num1 - num2;
-    return result;
+  let result = num1 - num2;
+  return result;
 }
 //multiply function
 const multiplication = function(num1, num2) {
-    let result = num1 * num2;
-    return result;
+  let result = num1 * num2;
+  return result;
 }
 //divide function
 const division = function (num1, num2) {
-    let result = num1/num2;
-    return result;
+  if (num2 === 0) {
+    alert("Do not divide by 0!")
+    return 0;
+  }
+  let result = num1/num2;
+  return result;
 }
-//create variables for number, operator and another number
+
+//declare variables to call elements
+const display = document.getElementById("display");
+const numbers = document.querySelectorAll(".gridbox");
+const operators = document.querySelectorAll(".gridbox-operator")
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
 let firstNumber = [];
 let secondNumber = [];
-let operator = [];
+let op = [];
+let arrayTrigger = 0;
+let result;
+let prevResult = [];
 
 //create new function operate that takes the above + calls operator on the numbers
 const operate = firstNumber => operator => secondNumber => {switch(operator) {
-    case '+': return addition(firstNumber,secondNumber);
-    case '-': return subtraction(firstNumber,secondNumber);
-    case '/': return division(firstNumber,secondNumber);
-    case '*': return multiplication(firstNumber,secondNumber);
-    default: return 'Invalid operation'
-  }}
-//create a variable to get the numbers by ID & initialise arrayTrigger to create numbers
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const divide = document.getElementById("4");
-const four = document.getElementById("5");
-const five = document.getElementById("6");
-const six = document.getElementById("7");
-const times = document.getElementById("8");
-const seven = document.getElementById("9");
-const eight = document.getElementById("10");
-const nine = document.getElementById("11");
-const minus = document.getElementById("12");
-const zero = document.getElementById("13");
-const clear = document.getElementById("14");
-const equals = document.getElementById("15");
-const add = document.getElementById("16");
-const display = document.getElementById("display");
-const buttons = document.querySelectorAll(".gridbox");
-let arrayTrigger = 0;
+  case '+': return addition(firstNumber,secondNumber);
+  case '-': return subtraction(firstNumber,secondNumber);
+  case '/': return division(firstNumber,secondNumber);
+  case '*': return multiplication(firstNumber,secondNumber);
+  default: return 'Invalid operation'
+}}
 
-//function to append number or symbol to display
-function appendtoDisplay(input) {
-    display.innerHTML += input;
-}
-
-//function to clear full display -> will be on AC button
-function clearDisplay() {
-    display.innerHTML = "";
-}
-
-//create array function to make each number
+//function to create array for first and second number
 function createArray(arr, num) {
-    arr.push(num);
-    return arr;
+  arr.push(num);
+  return arr;
 }
 
-//trigger for second array to start being made -> on each symbol
-function convertToTrue() {
-  arrayTrigger += 1;
+//clear the display function
+function clearDisplay() {
+  display.value = "";
+}
+
+//function to increment the array trigger
+function incrementArrayTrigger() {
+  arrayTrigger++;
+}
+
+//function to clear the array variables
+function clearArray(arr) {
+  arr.length = 0;
+  return arr;
+}
+
+//function to return the array trigger to 0
+function returnToDefault() {
+  arrayTrigger = 0;
+  return arrayTrigger;
 }
 
 //function to convert operator array into a string for reading in operate
@@ -85,148 +86,84 @@ function convertToNumber(arr1) {
   return number;
 }
 
-//function to return the array trigger to 0
-function returnToDefault() {
-  arrayTrigger = 0;
-  return arrayTrigger;
+//function to append number or symbol to display
+function appendtoDisplay(input) {
+  display.value += input;
 }
 
-//function to clear the variable arrays 
-function clearArray(arr) {
-  arr.length = 0;
-  return arr;
-}
-
-//add event listener to show the number on the display & create variables
-one.addEventListener('click', function () {
-  appendtoDisplay("1");
-  if (arrayTrigger === 0) {
-    createArray(firstNumber, 1);
-  } else if (arrayTrigger === 1) {
-    createArray(secondNumber, 1);
+Array.prototype.forEach.call(numbers,(number) => {
+  number.addEventListener("input", () => {
+  if (arrayTrigger % 2 === 0) {
+    numbers.disabled = true;
+  } else {
+    numbers.disabled = false;
   }
-});
-
-
-
-two.addEventListener('click', function() {
-    appendtoDisplay("2");
-    if (arrayTrigger === 0) {
-      createArray(firstNumber, 2);
-    } else if (arrayTrigger === 1) {
-      createArray(secondNumber, 2);
-    }
-});
-
-three.addEventListener('click', function () {
-    appendtoDisplay("3");
-    if (arrayTrigger === 0) {
-      createArray(firstNumber, 3);
-    } else if (arrayTrigger === 1) {
-      createArray(secondNumber, 3);
-    }
-  });
-
-divide.addEventListener('click', function () {
-  appendtoDisplay("/");
-  convertToTrue();
-  createArray(operator,"/");
-});
-
-four.addEventListener('click', function () {
-    appendtoDisplay("4");
-    if (arrayTrigger === 0) {                                                                                                             
-      createArray(firstNumber, 4);
-    } else if (arrayTrigger === 1) {
-      createArray(secondNumber, 4);
-    }
-  });
-  
-five.addEventListener('click', function () {
-    appendtoDisplay("5");
-    if (arrayTrigger === 0) {
-      createArray(firstNumber, 5);
-    } else if (arrayTrigger === 1) {
-      createArray(secondNumber, 5);
-    }
-  });
-
-six.addEventListener('click', function () {
-    appendtoDisplay("6");
-    if (arrayTrigger === 0) {
-      createArray(firstNumber, 6);
-    } else if (arrayTrigger === 1) {
-      createArray(secondNumber, 6);
-    }
-  });
-
-times.addEventListener('click', function () {
-    appendtoDisplay("*");
-    convertToTrue();
-    createArray(operator,"*");
-  });
-
-seven.addEventListener('click', function () {
-    appendtoDisplay("7");
-    if (arrayTrigger === 0) {
-      createArray(firstNumber, 7);
-    } else if (arrayTrigger === 1) {
-      createArray(secondNumber, 7);
-    }
-  });
-  
-eight.addEventListener('click', function() {
-      appendtoDisplay("8");
-      if (arrayTrigger === 0) {
-        createArray(firstNumber, 8);
-      } else if (arrayTrigger === 1) {
-        createArray(secondNumber, 8);
-      }
-  });
-  
-nine.addEventListener('click', function () {
-      appendtoDisplay("9");
-      if (arrayTrigger === 0) {
-        createArray(firstNumber, 9);
-      } else if (arrayTrigger === 1) {
-        createArray(secondNumber, 9);
-      }
-    });
-  
-minus.addEventListener('click', function () {
-    appendtoDisplay("-");
-    convertToTrue();
-    createArray(operator,"-");
-  });
-  
-zero.addEventListener('click', function () {
-      appendtoDisplay("0");
-      if (arrayTrigger === 0) {
-        createArray(firstNumber, 0);
-      } else if (arrayTrigger === 1) {
-        createArray(secondNumber, 0);
-      }
-    });
-    
-equals.addEventListener('click', function () {
-      clearDisplay();
-      let num1 = convertToNumber(firstNumber);
-      let num2 = convertToNumber(secondNumber);
-      let op = convertToString(operator);
-      let result = operate(num1)(op)(num2);
-      appendtoDisplay(result);
-    });
-  
-add.addEventListener('click', function () {
-      appendtoDisplay("+");
-      convertToTrue();
-      createArray(operator,"+");
-    });
-
-clear.addEventListener('click', function () {
-    clearDisplay();
-    returnToDefault();
-    clearArray(firstNumber);
-    clearArray(secondNumber);
-    clearArray(operator);
 })
+})
+
+//makes number buttons work
+Array.prototype.forEach.call(numbers,(number) => {
+  number.addEventListener("click", () => {
+    //creates array depending on what has been pressed
+    if (arrayTrigger === 0) {
+      display.value += number.innerText;
+      createArray(firstNumber, number.innerText);
+      console.log(firstNumber);
+    } else if (arrayTrigger % 2 != 0) {
+      display.value += number.innerText;
+      createArray(secondNumber, number.innerText);
+      console.log(secondNumber);
+    }
+  })
+})
+
+//makes operator buttons work
+Array.prototype.forEach.call(operators,(operator) => {
+  operator.addEventListener("click", () => {
+    display.value += operator.innerText;
+    createArray(op, operator.innerText);
+    console.log(op);
+    if (op.length > 1) {
+      clearDisplay();
+      display.value += "Invalid Operation"
+    }
+    incrementArrayTrigger();
+  })
+})
+
+//make clear button work
+clear.addEventListener('click', function () {
+  returnToDefault();
+  clearDisplay();
+  clearArray(firstNumber);
+  clearArray(secondNumber);
+  clearArray(op);
+  prevResult = [];
+})
+
+equals.addEventListener('click', function () {
+  clearDisplay();
+  incrementArrayTrigger();
+  if (prevResult.length === 0) {
+    let num1 = convertToNumber(firstNumber);
+    let num2 = convertToNumber(secondNumber);
+    let opString = convertToString(op);
+    result = operate(num1)(opString)(num2);
+    prevResult = result;
+    appendtoDisplay(result);
+  } else {
+    let num1 = prevResult;
+    let num2 = convertToNumber(secondNumber);
+    let opString = convertToString(op);
+    result = operate(num1)(opString)(num2);
+    prevResult = result;
+    appendtoDisplay(result);
+  }
+  console.log("Array Trigger:" + arrayTrigger);
+  console.log(result)
+  clearArray(secondNumber);
+  clearArray(op);
+  return prevResult;
+});
+
+console.log(prevResult)
